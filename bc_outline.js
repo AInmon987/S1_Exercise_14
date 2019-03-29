b2d4dd0bfb03fc820fc8d345665adc86251c7c77
austin inmon <Ainmon987@west-mec.org>
2019-03-29 14:39:11 -0700

    Created a heading and an semantic list.
    Created a heading and an semantic list. Added the heading for the outline. Looped through all of the child nodes of source article until no child nodes are left. Added an sequential ordered list.

--- bc_outline.js
+++ bc_outline.js
@@ -33,4 +33,47 @@ function makeOutline() {
       var outline = document.getElementById("outline");
       //source document for outline 
       var source = document.getElementById("doc");
+
+      var mainHeading = document.createElement("h1");
+      var outlineList = document.createElement("ol");
+      var headingText = document.createTextNode("Outline");
+
+      mainHeading.appendChild(headingText);
+      outline.appendChild(mainHeading);
+      outline.appendChild(outlineList);
+      createList(source, outlineList);
+}
+
+function createList(source, outlineList) {
+      //heading for the outline 
+      var headings = ["H1", "H2", "H3", "H4", "H5", "H6"]
+      //previous level of the headings
+      var prevLevel = 0;
+      //loop through all of the child nodes of source article until no child nodes are left 
+      for (var n = source.firstChild; n !== null; n = n.nextSibling) {
+            var heaadLevel = headings.indexOf(n.nodeName);
+            if (heaadLevel !== -1) {
+                  var listElem = document.createElement("li");
+                  listElem.innerHTML = n.firstChild.nodeValue;
+                  if (heaadLevel === prevLevel) {
+                        //append the list item to the cuurent list.  
+                        outlineList.appendChild(listElem);
+                  } else if (heaadLevel > prevLevel) {
+                        //start a new nested list 
+                        var nestedList = document.createElement("ol");
+                        nestedList.appendChild(listElem);
+                        //append nested list to last item in current list
+                        outlineList.lastChild.appendChild(nestedList);
+                        // Change the current list to the nested list 
+                        outlineList = nestedList;
+                  } else {
+                        //apend the list item to a higher list
+
+                  }
+
+                  //update the value of prevLevel
+                  prevLevel = heaadLevel;
+
+            }
+      }
 }
